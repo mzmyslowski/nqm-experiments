c3a6c07bd1742616a312a3915a27127f524b69f0
Michal Zmyslowski <schizoburger@gmail.com>
2020-04-09 10:13:52 +0200

    Added global path functionality to the experiments factory.

--- experiments_factory.py
+++ experiments_factory.py
@@ -21,6 +21,8 @@ MODEL_HYPERPARAMETERS = 'model_hyperparameters'
 OPTIMISER_HYPERPARAMETERS = 'optimiser_hyperparameters'
 TRAINING_HYPERPARAMETERS = 'training_hyperparameters'
 
+DATA_PATH = 'data'
+
 
 def make_dir(name: str):
     if not os.path.exists(name):
@@ -34,50 +36,60 @@ def get_experiments_outline() -> List[Dict]:
     return experiments_outline
 
 
-def get_new_model_path(experiment_name: str) -> str:
-    return '{}/{}_MODEL.pt'.format(experiment_name, experiment_name)
-
-
-def copy_file(old_path: str, new_path: str) -> None:
-    shutil.copyfile(old_path, new_path)
-
-
-def save_experiment_dict(experiment_dict_):
-    dir_files_names = os.listdir(experiment_dict_[EXPERIMENT_NAME])
-    dir_files_names_filtered = list(filter(lambda x: 'arguments' in x and 'json' in x, dir_files_names))
+def save_experiment_dict(experiment_dict_, path):
+    dir_files_names_filtered = list(filter(lambda x: 'arguments' in x and 'json' in x, path))
     dumped_dict = json.dumps(experiment_dict_)
-    file_name = '{}/arguments_{}.json'.format(experiment_dict_[EXPERIMENT_NAME], len(dir_files_names_filtered))
+    file_name = '{}/arguments_{}.json'.format(path, len(dir_files_names_filtered))
     f = open(file_name, "w")
     f.write(dumped_dict)
     f.close()
 
 
-def init_experiment_env(experiment_dict_):
+def init_experiment_env(experiment_dict_, path):
     name = experiment_dict_[EXPERIMENT_NAME]
     print('Initialising an environment for the experiment: {}'.format(name))
-    make_dir(name=name)
-    save_experiment_dict(experiment_dict_=experiment_dict_)
+    experiment_path = os.path.join(path, name)
+    make_dir(name=experiment_path)
+    save_experiment_dict(experiment_dict_=experiment_dict_, path=experiment_path)
+    return experiment_path
 
 
-def perform_experiment(experiment_dict_):
-    train_model(experiment_dict_=experiment_dict_)
+def perform_experiment(experiment_dict_, experiment_path, data_path):
+    train_model(experiment_dict_=experiment_dict_, experiment_path=experiment_path, data_path=data_path)
 
 
-def train_model(experiment_dict_: Dict):
+def train_model(experiment_dict_: Dict, experiment_path, data_path):
     dataset_generator = datasets_loading_factory.DatasetGeneratorPyTorch(
         **experiment_dict_[DATASET_HYPERPARAMETERS],
+        data_path=data_path
     )
-    k_matrix_factory = derivatives_factory.KMatrixFactory(
-        dataset_generator=dataset_generator, **experiment_dict[K_HYPERPARAMETERS]
+    if experiment_dict_.get(K_HYPERPARAMETERS) is not None:
+        k_matrix_factory = derivatives_factory.KMatrixFactory(
+            dataset_generator=dataset_generator, **experiment_dict[K_HYPERPARAMETERS]
+        )
+    else:
+        k_matrix_factory = None
+    if experiment_dict_.get(H_HYPERPARAMETERS) is not None:
+        hessian_factory = derivatives_factory.HessianFactory(
+            dataset_generator=dataset_generator, **experiment_dict[H_HYPERPARAMETERS]
+        )
+    else:
+        hessian_factory = None
+    model_factory_ = models_factory.ModelsFactory(
+        **experiment_dict_[MODEL_HYPERPARAMETERS],
+        path_to_save=experiment_path
     )
-    hessian_factory = derivatives_factory.HessianFactory(
-        dataset_generator=dataset_generator, **experiment_dict[H_HYPERPARAMETERS]
+    optimiser_factory = optimisation_factory.OptimisersFactoryPyTorch(
+        **experiment_dict_[OPTIMISER_HYPERPARAMETERS],
+        path_to_save=experiment_path
     )
-    model_factory_ = models_factory.ModelsFactory(**experiment_dict_[MODEL_HYPERPARAMETERS])
-    optimiser_factory = optimisation_factory.OptimisersFactoryPyTorch(**experiment_dict_[OPTIMISER_HYPERPARAMETERS])
-    metrics_factory_ = metrics_factory.MetricsFactory(**experiment_dict_[METRICS_HYPERPARAMETERS])
+    if experiment_dict_.get(METRICS_HYPERPARAMETERS) is not None:
+        metrics_factory_ = metrics_factory.MetricsFactory(**experiment_dict_[METRICS_HYPERPARAMETERS])
+    else:
+        metrics_factory_ = None
     training_experiment = rnn_training_experiments.RNNTrainingExperimentPyTorch(
         **experiment_dict_[TRAINING_HYPERPARAMETERS],
+        experiment_path=experiment_path,
         dataset_generator=dataset_generator,
         models_factory=model_factory_,
         optimisers_factory=optimiser_factory,
@@ -89,8 +101,10 @@ def train_model(experiment_dict_: Dict):
 
 
 if __name__ == '__main__':
+    path = input('Please specify global path for experiments: ')
+    data_path = os.path.join(path, DATA_PATH)
     experiments_outline = get_experiments_outline()
-    print('Starting performing experiments.')
+    print('Performing experiments.')
     for experiment_dict in experiments_outline:
-        init_experiment_env(experiment_dict_=experiment_dict)
-        perform_experiment(experiment_dict_=experiment_dict)
+        experiment_path = init_experiment_env(experiment_dict_=experiment_dict, path=path)
+        perform_experiment(experiment_dict_=experiment_dict, experiment_path=experiment_path, data_path=data_path)
